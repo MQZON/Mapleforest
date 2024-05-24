60e8b09cbace8be3fff8165fb38c9f18e7d750cc
MQZON <mxwllsky@gmail.com>
2024-05-24 15:04:50 -0700

    Create main mod file and class

--- src/main/java/net/mqzon/mapleforest/Mapleforest.java
+++ src/main/java/net/mqzon/mapleforest/Mapleforest.java
@@ -1,4 +1,4 @@
-package net.mqzon.modtemplate;
+package net.mqzon.mapleforest;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -7,24 +7,22 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ModTemplate.MOD_ID)
-public class ModTemplate
+@Mod(Mapleforest.MOD_ID)
+public class Mapleforest
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "modtemplate";
+    public static final String MOD_ID = "mapleforest";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ModTemplate()
+    public Mapleforest()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
